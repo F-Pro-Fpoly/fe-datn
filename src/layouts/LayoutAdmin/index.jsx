4a660064b629dcd95b40ebaae0ace6f89f710a8f
Van Phan <phanvan0322@gmail.com>
2022-09-19 13:21:05 +0700

    NavBarTop

--- src/layouts/LayoutAdmin/index.jsx
+++ src/layouts/LayoutAdmin/index.jsx
@@ -3,14 +3,24 @@ import NavBarAdmin from "../../components/admin/NavBarAdmin";
 import NavBarTop from "../../components/admin/NavBarTop/NavBarTop";
 
 import "./LayoutAdmin.scss";
+import { useState } from "react";
+import { useEffect, useRef } from "react";
 
 function LayoutAdmin() {
+    const [navEl2, setNavEl2] = useState();
+    const adminRef = useRef();
+
+    useEffect(()=>{
+        setNavEl2(adminRef.current)
+    },[])
+
+
     return ( 
         <>
-        <div className="admin">
-            <NavBarAdmin className="admin-aside" />
+        <div className="admin" ref={adminRef}>
+            <NavBarAdmin className="admin-aside"  />
             <main className="admin-main">
-                <NavBarTop />
+                <NavBarTop navEl2={navEl2} />
                 <Outlet />
             </main>
         </div>
