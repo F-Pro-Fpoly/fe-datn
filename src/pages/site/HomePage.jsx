a3df34d58443adb2e5c818c3cd445255e40f70d4
Thanh Phu <phuly4795@gmail.com>
2022-09-19 11:06:26 +0700

    abc

--- src/pages/site/HomePage.jsx
+++ src/pages/site/HomePage.jsx
@@ -9,7 +9,7 @@ function HomePage() {
             {
                 user&&
                 <div>
-                    {user.name}    
+                    {user.name}
                 </div>
             }
 
