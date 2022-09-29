c156d7058670c5bef8ac374a1d22c58843dece84
Van Phan <phanvan0322@gmail.com>
2022-09-29 15:52:02 +0700

    list user api

--- src/pages/admin/User/ListUser/ListUser.jsx
+++ src/pages/admin/User/ListUser/ListUser.jsx
@@ -1,6 +1,38 @@
+import { useState } from 'react';
+import { useEffect } from 'react';
 import Table from 'react-bootstrap/Table';
+import {getListUsersAPI} from "../../../../services/UserService";
+import {useDispatch, useSelector} from "react-redux";
+import Loading from '../../../../components/Loading/Loading';
+
+import "./ListUser.scss";
+
+
 function ListUser() {
+  const token = useSelector(state => state.auth.token);
+
+  const [listUser, getListUser] = useState([]);
+  const [loading, getLoading] = useState(false);
+
+
+  useEffect(()=>{
+    const start = async () =>{
+      getLoading(true);
+      let res = await getListUsersAPI(token);
+      let data = res.data;
+      let dataArr = data.data;
+
+      getLoading(false);
+      getListUser(dataArr);
+    }
+
+    start();
+  },[]);
+
+
+
   return (
+    <>
     <Table striped bordered hover>
       <thead>
         <tr>
@@ -10,42 +42,42 @@ function ListUser() {
           <th>Username</th>
           <th>Email</th>
           <th>Address</th>
-          <th>Active</th>
           <th>Phone</th>
+          <th>Active</th>
           <th>Avata</th>
           <th>Edit</th>
           <th>Delete</th>
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td></td>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-          <td>222 Ngô Thì Nhậm </td>
-          <td></td>
-          <td>099292222</td>
-          <td>hinh1.pnv</td>
-          <td><i className="fas fa-edit"></i></td>
-          <td><i className="fa fa-trash"></i></td>
-        </tr>
-        <tr>
-        <td></td>
-          <td>2</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-          <td>222 Ngô Thì Nhậm </td>
-          <td></td>
-          <td>099292222</td>
-          <td>hinh1.pnv</td>
-          <td><i className="fas fa-edit"></i></td>
-          <td><i className="fa fa-trash"></i></td>
-        </tr>
+        {
+          listUser.map((val, index)=>(
+            <tr key={index}>
+              <td></td>
+              <td>{index+1}</td>
+              <td>{val.name}</td>
+              <td>{val.username}</td>
+              <td>{val.email}</td>
+              <td>{val.address ?? null}</td>
+              <td>{val.phone}</td>
+              <td>{val.active === 1 ?"Đang kích hoạt":"Ngừng kích hoạt"}</td>
+              <td >
+                <img className='listUser-img' src={val.avatar} alt="Ảnh user" />
+              </td>
+              <td><i className="fas fa-edit"></i></td>
+              <td><i className="fa fa-trash"></i></td>
+            </tr>
+          ))
+        }
+        
+        
       </tbody>
+      
     </Table>
+    {
+      loading && <Loading />
+    }
+    </>
   );
 }
 
