7e0791b5b36c0a564aab25cb7cd34bc85c71f1f7
Van Phan <phanvan0322@gmail.com>
2022-10-08 21:54:22 +0700

    add user

--- src/pages/admin/User/AddUser/AddUser.jsx
+++ src/pages/admin/User/AddUser/AddUser.jsx
@@ -1,65 +1,88 @@
+// import { get } from 'immer/dist/internal';
+import { useEffect, useRef } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import {useDispatch, useSelector} from "react-redux";
+import { toast,ToastContainer } from 'react-toastify';
+
+
+import {createUserApi} from "../../../../services/UserService"
 
 function AddUser() {
+  const token = useSelector((state)=>state.auth.token);
+  const FormRep = useRef();
+  const submitForm = async (event) =>{
+    event.preventDefault();
+    const formData = new FormData(FormRep.current);
+    const req = {
+      "token": token,
+      "data": formData
+    };
+    try {
+      let res = await createUserApi(req);
+
+      toast.success(res.data.message) ;     
+    } catch (error) {
+      let res = error.response;
+      let data = res.data;
+      let messages = data.message;
+      toast.error(messages);
+    }
+
+  }
+
+
   return (
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form onSubmit={submitForm} ref={FormRep} enctype='multipart/form-data'>
+      <ToastContainer
+        position="top-right"
+        autoClose={4000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        />
+        {/* Same as */}
+      <ToastContainer />
+
+
+      <Form.Group className="mb-3" controlId="FormUsername">
+        <Form.Label>Tên đăng nhập</Form.Label>
+        <Form.Control type="text" name="username" placeholder="Nhập username" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formPassword">
+        <Form.Label>Mật khẩu</Form.Label>
+        <Form.Control type="password" name="password" placeholder="Nhập mật khẩu"  />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formEmail">
         <Form.Label>Email</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
+        <Form.Control type="email" name="email" placeholder="Nhập Email"  />
       </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicName">
-        <Form.Label>Name </Form.Label>
-        <Form.Control type="text" placeholder="Enter Name" />
-        <Form.Text className="text-muted">
-          We'll never share your Name with anyone else.
-        </Form.Text>
+      <Form.Group className="mb-3" controlId="formName">
+        <Form.Label>Họ và tên</Form.Label>
+        <Form.Control type="text" name="name" placeholder="Nhập họ và tên"  />
       </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicUsername">
-        <Form.Label>Username</Form.Label>
-        <Form.Control type="text" placeholder="Enter Username" />
-        <Form.Text className="text-muted">
-          We'll never share your Username with anyone else.
-        </Form.Text>
+      <Form.Group className="mb-3" controlId="formActive">
+        <Form.Label>Kích hoạt</Form.Label>
+        <Form.Select aria-label="Default select example" name="active">
+          <option>--Chọn--</option>
+          <option value="1">Kích hoạt</option>
+          <option value="0">Không kích hoạt</option>
+        </Form.Select>
       </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicAddress">
-        <Form.Label>Address</Form.Label>
-        <Form.Control type="Text" placeholder="Enter Address" />
-        <Form.Text className="text-muted">
-          We'll never share your Address with anyone else.
-        </Form.Text>
+      <Form.Group className="mb-3" controlId="formRole">
+        <Form.Label>Chức vụ</Form.Label>
+        <Form.Select aria-label="Default select example" name="role_id">
+          <option>--Chọn--</option>
+          <option value="1">Admin</option>
+          <option value="2">Bác sĩ</option>
+          <option value="3">Người dùng</option>
+        </Form.Select>
       </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicActive">
-        <Form.Label>Active</Form.Label>
-        <Form.Control type="Text" placeholder="Enter Active" />
-        <Form.Text className="text-muted">
-          We'll never share your Active with anyone else.
-        </Form.Text>
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicPhone">
-        <Form.Label>Phone</Form.Label>
-        <Form.Control type="Text" placeholder="Enter Phone" />
-        <Form.Text className="text-muted">
-          We'll never share your Phone with anyone else.
-        </Form.Text>
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicAvata">
-        <Form.Label>Avata</Form.Label>
-        <Form.Control type="file" placeholder="Enter Avata" />
-        <Form.Text className="text-muted">
-          We'll never share your Avata with anyone else.
-        </Form.Text>
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
+      
       <Button variant="primary" type="submit">
         Submit
       </Button>
