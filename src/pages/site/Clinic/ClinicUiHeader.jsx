66fd60897abbae919c15db4437b074acd9698cf5
Van Phan <phanvan0322@gmail.com>
2022-11-14 15:43:30 +0700

    ok

--- src/pages/site/Clinic/ClinicUiHeader.jsx
+++ src/pages/site/Clinic/ClinicUiHeader.jsx
@@ -1,11 +1,24 @@
+import { useState } from "react";
+
 function ClinicUiHeader({specialist = {}}) {
+    const [showAll, setShowAll] = useState(false);
+
+    const handleOnClick = () => {
+        if(showAll){
+            setShowAll(false)
+            return
+        }
+        setShowAll(true)
+        return
+    }
+
     return ( 
         <div className="infoClinic">
             <div className="text">
                 <div className="container">
                     <h3>{specialist.name}</h3>
-                    <div className="">
-                        {specialist.description}
+                    <div className={`infoClinic-content ${showAll ? 'active':''}`} onClick={handleOnClick} dangerouslySetInnerHTML={{__html: specialist.description}}>
+                        
                     </div>
                 </div>
             </div>
