777834883370bd77e7cdb2ae1f491deced9bc239
Van Phan <phanvan0322@gmail.com>
2022-11-16 20:36:38 +0700

    ok

--- src/pages/site/Vaccine/Vaccine.jsx
+++ src/pages/site/Vaccine/Vaccine.jsx
@@ -1,39 +1,64 @@
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
 import "./Vaccine.scss"
+import VaccineContent from "./VaccineContent";
 
 function Vaccine() {
+
+    useEffect(() => {
+        document.title = "Trang vaccine";
+    }, [])
+
+
     return ( 
         <div className="vaccine">
             <section className="vaccine-title">
                 <div className="container">
-                    <div className="row">
-                        <div className="vaccine-title-item">
-                            <div className="vaccine-title-category">
-                                <button>
-                                    <i className="fa-solid fa-bars"></i>
-                                    <span>DANH MỤC</span>
-                                </button>
+                    <div className="d-flex justify-content-between vaccine-title-row">
+                        <div className="d-flex vaccine-title-left">
+                            <div className="vaccine-title-item">
+                                <div className="vaccine-title-category">
+                                    <button type="button">
+                                        <i className="fa-solid fa-bars"></i>
+                                        <span>DANH MỤC</span>
+                                    </button>
+                                    <ul className="vaccine-title-list">
+                                        <li className="vaccine-title-li">
+                                            <Link to={`/`}>Vaccine cho trẻ em</Link>
+                                        </li>
+                                        <li className="vaccine-title-li">
+                                            <Link to={`/`}>Vaccine cho trẻ tiền học đường</Link>
+                                        </li>
+                                        <li className="vaccine-title-li">
+                                            <Link to={`/`}>Vaccine cho trẻ vị thành niên</Link>
+                                        </li>
+                                        <li className="vaccine-title-li">
+                                            <Link to={`/`}>Vaccine cho người trưởng thành</Link>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className="vaccine-title-item">
+                                <span className="vaccine-title-h3">
+                                    THÔNG TIN SẢN PHẨM VẮC XIN
+                                </span>
                             </div>
                         </div>
-                        <div className="vaccine-title-item">
-                            <h3 className="vaccine-title-h3">
-                                THÔNG TIN SẢN PHẨM VẮC XIN
-                            </h3>
-                        </div>
-                        <div className="vaccine-title-item">
-                            <form className="vaccine-title-search">
-                                <input type="text" className="" />
-                                <button type="button">
-                                    <i className="fa-solid fa-magnifying-glass"></i>
-                                </button>
-                            </form>
+                        <div className="d-flex vaccine-title-right">
+                            <div className="vaccine-title-item" >
+                                <form className="vaccine-title-search">
+                                    <input type="text" className="" placeholder="Tìm kiếm tên vaccine" />
+                                    <button type="button">
+                                        <i className="fa-solid fa-magnifying-glass"></i>
+                                    </button>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
             </section>
 
-            <section className="vaccine-content">
-
-            </section>
+            <VaccineContent />
         </div>
      );
 }
