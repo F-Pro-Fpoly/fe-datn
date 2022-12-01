016a109d0fc4052fec249bb5ac3d8d73ca01448b
Thanh Phu <phuly4795@gmail.com>
2022-12-01 10:21:35 +0700

    a

--- src/pages/admin/Contact/ListContactBooking/ListContactBooking.jsx
+++ src/pages/admin/Contact/ListContactBooking/ListContactBooking.jsx
@@ -5,7 +5,7 @@ import Table from "react-bootstrap/esm/Table";
 import { useSelector } from "react-redux";
 
 // import "./listContac.scss";
-import { getContactBooking } from "../../../../services/ContactService";
+import { getContact } from "../../../../services/ContactService";
 import Paginate from "../../../../components/Paginate/Paginate";
 import Loading from "../../../../components/Loading/Loading";
 
@@ -18,13 +18,17 @@ function ListContactBooking() {
     const [loading, getLoading] = useState(false);
     const [paginate, setPaginate] = useState(null);
     const [page, setPage] = useState(1);
-    
+    const [search, setSearch] = useState({
+      "status" : ""
+    });
+
+    const type = 1;
     useEffect(() => {
       
         const start = async () => {
             getLoading(true)
             setListContact([])
-            let res = await getContactBooking({token,page}) 
+            let res = await getContact({token,page,search, type}) 
             let data = res.data 
             let dataArr = data.data
             getLoading(false)
@@ -42,9 +46,37 @@ function ListContactBooking() {
       setPage(number);
     }
 
+    const HandleSearch = async (value) => {
+      value.preventDefault();
+      getLoading(true)
+      setListContact([])
+      let res = await getContact({token,page,search,type}) 
+      let data = res.data 
+      let dataArr = data.data
+      getLoading(false)
+      setListContact(dataArr)
+      // handle paginate
+      let pagination = data.meta.pagination ?? null;
+      setPaginate(pagination);
+    }
+
     return ( 
         <>
         <div className="adminItem">
+        <div className='row mt-3 mb-3' >
+            <div className="col-2 form-group">
+                <select name="status" id="" defaultValue={0}
+                  onChange={(e)=>setSearch({...search, "status": e.target.value})}
+                className="form-control">
+                    <option value="0" disabled>Chọn Trạng thái</option>
+                    <option value="8">Đã phản hồi</option>
+                    <option value="9">Chưa phản hồi</option>
+                </select>
+            </div>
+            <div className="col-2">
+              <button className='btn btn-primary' onClick={HandleSearch} >Tìm kiếm</button>
+            </div>
+          </div>
           <Table striped bordered hover className='table-striped'>
             <thead>
               <tr>
@@ -64,7 +96,11 @@ function ListContactBooking() {
                     <td>{index+1}</td>
                     <td>{val.name}</td>
                     <td >{val.email} </td>             
-                    <td>{val.status}</td>
+                    {val.status_id == 9 ? 
+                      <td style={{color:"red"}}>{val.status}</td> 
+                      :  
+                      <td style={{color:"green"}}>{val.status}</td>
+                    }
                     <td>{val.created_at}</td>
                     <td className="button">
                     <Link to={`/admin/lien-he/tra-loi-dang-ky-lich/${val.id}`} className="btn">
