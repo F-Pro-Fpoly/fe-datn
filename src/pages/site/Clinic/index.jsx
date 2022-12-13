76f4f6694d0b94d1498e8b2f7189274516bd4ed0
Thanh Phu <phuly4795@gmail.com>
2022-12-13 12:43:42 +0700

    a

--- src/pages/site/Clinic/index.jsx
+++ src/pages/site/Clinic/index.jsx
@@ -38,6 +38,7 @@ function Clinic () {
     return (
        <>
 
+    
         <ToastContainer />
         <ClinicUiHeader specialist={specialist} />     
         <ClinicUiContent listDoctor={listDoctor} />
